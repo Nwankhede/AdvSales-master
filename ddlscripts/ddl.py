15aa05b841d308aa634af25466d15f84648920c2
Nitin Wankhede <classyclicks7@gmail.com>
2023-05-21 22:34:48 +0530

    Dim load tables completed

--- ddlscripts/ddl.py
+++ ddlscripts/ddl.py
@@ -28,3 +28,52 @@ load_time timestamp
 )
 stored AS PARQUET; """
 # create other dim tables take reference of ssp_dim creation
+adv_deal = """CREATE TABLE IF NOT EXISTS deal_dim(
+ deal_id bigint,
+ adv_deal  string)
+PARTITIONED BY ( 
+load_time timestamp
+)
+stored AS PARQUET; """
+advertiser = """CREATE TABLE IF NOT EXISTS advertiser_dim(
+ advertiser_id bigint,
+ advertiser  string)
+PARTITIONED BY ( 
+load_time timestamp
+)
+stored AS PARQUET; """
+country = """CREATE TABLE IF NOT EXISTS country_dim(
+ country_id bigint,
+ country  string)
+PARTITIONED BY ( 
+load_time timestamp
+)
+stored AS PARQUET; """
+adv_property = """CREATE TABLE IF NOT EXISTS property_identifier_dim(
+ property_id bigint,
+ property  string)
+PARTITIONED BY ( 
+load_time timestamp
+)
+stored AS PARQUET; """
+device_category = """CREATE TABLE IF NOT EXISTS device_category_dim(
+ device_id bigint,
+ device  string)
+PARTITIONED BY ( 
+load_time timestamp
+)
+stored AS PARQUET; """
+adv_agency = """CREATE TABLE IF NOT EXISTS agency_dim(
+ agency_id bigint,
+ agency  string)
+PARTITIONED BY ( 
+load_time timestamp
+)
+stored AS PARQUET; """
+marketplace = """CREATE TABLE IF NOT EXISTS marketplace_dim(
+ marketplace_id bigint,
+ marketplace  string)
+PARTITIONED BY ( 
+load_time timestamp
+)
+stored AS PARQUET; """
